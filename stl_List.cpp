760b764dd4adc28088031d84c9b03dca4c4159e6
Osmangonibinismail <mdosmangoni4314@gmail.com>
2025-08-31 04:27:29 +0600

    first commit

---
+++ stl_List.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> l(10,5);
+    for(auto it = l.begin(); it!=l.end(); it++){
+        cout << *it << endl;
+    }
+    return 0;
+}
